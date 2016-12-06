f9635453a9fca346f08cbcf49579c683b559d784
mtdeguzis <mdeguzis@gmail.com>
2016-12-05 19:02:31 -0500

    mark exec

---
+++ systems-engineer/hadoop/create-single-node-simple.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# TODO
+
+###############
+# Components
+###############
+#
+#    Hadoop Common – it contains the Java libraries and utilities needed by other Hadoop modules.
+#    HDFS – Hadoop Distributed File System – A Java based scalable file system distributed across multiple nodes.
+#    MapReduce – YARN framework for parallel big data processing.
+#    Hadoop YARN: A framework for cluster resource management.
+
+
+# Set hostname
+# You can use "master" to be more in line with hadoop conventions
+hostnamectl set-hostname centos7
+
