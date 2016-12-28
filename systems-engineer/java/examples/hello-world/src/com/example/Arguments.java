47c43276c0fc878b276ee260f8ab4b1124f1760b
Michael T. DeGuzis <mdeguzis@users.noreply.github.com>
2016-12-28 08:14:03 -0500

    Create Arguments.java

---
+++ systems-engineer/java/examples/hello-world/src/com/example/Arguments.java
@@ -0,0 +1,12 @@
+/*
+
+Directions:
+  - compile with 'java Arguments', or "Run" with your IDE
+*/
+
+class Main { 
+
+  public static void main(String[] args) {       
+    // print some text       
+    System.out.println("May the Force be with you");          
+  }
