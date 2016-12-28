c4e42badd8156a4c63eedacd4accea67020e93e8
Michael T. DeGuzis <mdeguzis@users.noreply.github.com>
2016-12-28 08:11:08 -0500

    Update Main.java

--- systems-engineer/java/examples/hello-world/src/com/example/Main.java
+++ systems-engineer/java/examples/hello-world/src/com/example/Main.java
@@ -1,1 +1,20 @@
-// The filename of this code should match the classname// Access modifier public, available to entire application// Start class with uppercasepublic class Main { // static - member of class that can be called from the class itself   // void -do not return values   // start method names with lowercase   // inside parenthesis, declare arguments ([NAME] [ARGS])   // String[] is an array of strings due to []    public static void main(String[] args) {       // print some text       System.out.println("May the Force be with you");          }
+
+/*
+The filename of this code should match the classname
+Access modifier public, available to entire application
+Start class with uppercasepublic 
+
+Directions:
+  - compile with 'java Main', or "Run" with your IDE
+*/
+
+class Main { 
+  // static - member of class that can be called from the class itself   
+  // void -do not return values   
+  // start method names with lowercase   
+  // inside parenthesis, declare arguments ([NAME] [ARGS])   
+  // String[] is an array of strings due to []    
+  public static void main(String[] args) {       
+    // print some text       
+    System.out.println("May the Force be with you");          
+  }
