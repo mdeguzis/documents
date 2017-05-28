3c1b3ab6d5deebdeef295e2fef9e41c5be19039d
Michael T. DeGuzis <mdeguzis@users.noreply.github.com>
2017-05-28 12:38:13 -0400

    Create generate-app-toc.sh

---
+++ linux-applications/generate-app-toc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Description: Parses current directory for readme.md or the same name as the folder
+#              Found documents are pushed to a markdown-friendly file as a means of 
+#              having a TOC/Index of the contents.
+
+# TODO
